495652b074b3944bdbab1ad6fb6f3744a077058e
suniel12 <142744245+suniel12@users.noreply.github.com>
2023-08-20 11:45:06 -0700

    Update main.tf

--- main.tf
+++ main.tf
@@ -43,7 +43,7 @@ resource "abbey_grant_kit" "abbey_demo_site" {
   output = {
     # Replace with your own path pointing to where you want your access changes to manifest.
     # Path is an RFC 3986 URI, such as `github://{organization}/{repo}/path/to/file.tf`.
-    location = "github://replace-me-with-organization/replace-me-with-repo/access.tf" # CHANGEME
+    location = "github://suniel12/abbey/access.tf" # CHANGEME
     append = <<-EOT
       resource "abbey_demo" "grant_read_write_access" {
         permission = "read_write"
